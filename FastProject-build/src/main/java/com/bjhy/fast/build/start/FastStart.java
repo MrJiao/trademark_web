7ea2526a32c48e9284adcf7b215330d155e44a58
MrJiao <361606225@qq.com>
2019-07-03 22:13:54 +0800

    添加新的除水印功能

--- FastProject-build/src/main/java/com/bjhy/fast/build/start/FastStart.java
+++ FastProject-build/src/main/java/com/bjhy/fast/build/start/FastStart.java
@@ -2,10 +2,7 @@ package com.bjhy.fast.build.start;
 
 import org.apel.gaia.container.boot.PlatformStarter;
 
-/**
- *两级数据交换启动类
- *
- */
+
 public class FastStart {
 	
 	public static void main(String[] args) {
